b9a1f23e06ae7380a37bbb435cea0db6e6527ac7
shengchen-liu <shengchen.liu1225@gmail.com>
2023-10-10 02:11:18 -0700

    Implement MySlam

---
+++ MySlam/src/config.cpp
@@ -0,0 +1,24 @@
+#include "myslam/config.h"
+
+namespace myslam {
+bool Config::SetParameterFile(const std::string &filename) {
+    if (config_ == nullptr)
+        config_ = std::shared_ptr<Config>(new Config);
+    static cv::FileStorage file1( filename.c_str(), cv::FileStorage::READ );
+    config_->file_ = file1;
+    if (config_->file_.isOpened() == false) {
+        LOG(ERROR) << "parameter file " << filename << " does not exist.";
+        config_->file_.release();
+        return false;
+    }
+    return true;
+}
+
+Config::~Config() {
+    if (file_.isOpened())
+        file_.release();
+}
+
+std::shared_ptr<Config> Config::config_ = nullptr;
+
+}
